1c94325def905c4c31d79b18e97ecbc163dc2b05
GabrielRossi01 <rossi17006@gmail.com>
2025-09-04 09:42:49 -0300

    adding ValidationHandler and more infos

--- src/main/java/br/com/fiap/financewalk/repository/CategoryRepository.java
+++ src/main/java/br/com/fiap/financewalk/repository/CategoryRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.fiap.financewalk.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long>  {
-    
-    
+     
 }
