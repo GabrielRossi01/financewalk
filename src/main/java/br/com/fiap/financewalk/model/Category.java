7374c023dbdf793b6c507bc3062ab17350b42132
GabrielRossi01 <rossi17006@gmail.com>
2025-09-08 11:06:47 -0300

    implement validation for Category model with custom Icon annotation and messages

--- src/main/java/br/com/fiap/financewalk/model/Category.java
+++ src/main/java/br/com/fiap/financewalk/model/Category.java
@@ -1,9 +1,11 @@
 package br.com.fiap.financewalk.model;
 
+import br.com.fiap.financewalk.validation.Icon;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 //import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +19,11 @@ import lombok.NoArgsConstructor;
 public class Category {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "{category.name.notblank}")
     private String name;
+
+    @NotBlank(message = "{category.icon.notblank}")
+    @Icon
     private String icon;
 }
