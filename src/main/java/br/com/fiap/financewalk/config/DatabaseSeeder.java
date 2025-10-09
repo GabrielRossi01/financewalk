b531ae1834d85d54587f8d459f9cfd0a3ca5b9a9
GabrielRossi01 <rossi17006@gmail.com>
2025-10-09 09:40:53 -0300

    Add profile annotation to DatabaseSeeder for development environment

--- src/main/java/br/com/fiap/financewalk/config/DatabaseSeeder.java
+++ src/main/java/br/com/fiap/financewalk/config/DatabaseSeeder.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import br.com.fiap.financewalk.model.Category;
@@ -16,6 +17,7 @@ import br.com.fiap.financewalk.repository.TransactionRepository;
 import jakarta.annotation.PostConstruct;
 
 @Component
+@Profile("dev")
 public class DatabaseSeeder {
 
     @Autowired
